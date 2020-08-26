26306af375bcf5fafc1523253155e4a3ecad214d
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-08-26 17:13:34 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,7 +3,10 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   
   def change
     create_table :haunted_houses do |t|
-      
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.integer :price
   end
   
 end
